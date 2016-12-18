c35824d0382f072d991ceea55764f687d5ff0903
plaindruid <sepang3x@yahoo.com>
2016-12-18 16:22:58 +0800

    New Scene: Level_scene Sripts: Level_unlocker----ilalagay mo to sa trigger kung saan yung exit nga stage              Level_Selector --- UI to ng mga button pang select ng stage. see example sa level_scene              LevelSelectionManager--- nakalagay ito sa canvas ng level_scene

---
+++ Egyptian Treasure Quest Z/Assets/_MyScripts/Optimization/Scripts/Interactions/LevelSelection.cs
@@ -0,0 +1,51 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+public class LevelSelection : MonoBehaviour
+{
+    public int Stage_ID;
+    public Sprite Stage_image;
+    public string Stage_name;
+    bool unlock = false;
+    // Use this for initialization
+    void Start()
+    {
+        Stage_image = LevelSelectionManager.steplevelselection.Lock_Sprite;  
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        Stage_status();
+        Change_pic();  
+    }
+
+    public void stage_Click()
+    {
+        if(unlock)
+        {
+            SceneManager.LoadScene(Stage_name);  
+        }
+    }
+
+    void Stage_status()
+    {
+        if(Stage_ID != 0 & !unlock)
+        {
+            unlock = LevelSelectionManager.steplevelselection.CheckStageStatus(Stage_ID);   
+        }
+    }
+
+    void Change_pic()
+    {
+        if(unlock)
+        {
+            Stage_image = LevelSelectionManager.steplevelselection.Unlock_Sprite;    
+        }
+
+        GetComponent<Image>().sprite = Stage_image;   
+         
+    }
+}
