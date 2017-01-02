298eaa1499f9a4fb1f5d937f62470a88d25a9ffa
plaindruid <sepang3x@yahoo.com>
2017-01-02 14:05:26 +0800

    no message

---
+++ Egyptian Treasure Quest Z/Assets/_MyScripts/Optimization/Scripts/Interactions/GalleryImagePic.cs
@@ -0,0 +1,84 @@
+﻿/// <summary>
+/// Gallery image pic.
+/// Ilalagay mo to sa mga image na object sa Gallery UI mo
+/// 
+/// </summary>
+using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+public class GalleryImagePic : MonoBehaviour 
+{
+
+	public QuestDatabase Quest_Database;
+	public int Quest_ID;
+	public Sprite Lock_image; 
+	// Use this for initialization
+	void Start () 
+	{
+		GetComponent<Image> ().sprite = Lock_image;   
+	}
+	
+	// Update is called once per frame
+	void Update ()
+	{
+		if (CheckQuest (Quest_ID, QuestList._Quest_Status.Completed)) 
+		{
+			DisplayImage (); 
+		}
+
+	}
+
+	bool CheckQuest(int Quest_Id, QuestList._Quest_Status Quest_Status)
+	{
+		bool res1 = false;
+		//Debug.Log("dsfwefasfer");
+		foreach (QuestList ql in Quest_Database.Quests)
+		{
+			if(ql.Quest_ID == Quest_Id && ql.Quest_Status == Quest_Status)
+			{
+				if (ql.Gallery_opened)
+				{	 
+					res1 = true;
+				}
+			}
+		}
+
+		if(res1)
+		{
+			return true; //Return True kung sakto yung quest id and Quest status
+		}
+		else
+		{
+			return false;  //Return False kung hindi sakto yung quest id and Quest status
+		}
+
+	}
+
+	void DisplayImage()
+	{
+		if (GetComponent<Image> () != null) 
+		{
+			GetComponent<Image> ().sprite = get_picture (); 
+		} 
+		else
+		{
+			Debug.LogWarning ("Iha walang Image script component akong nakikita");
+		}
+	}
+
+	Sprite get_picture()
+	{
+		Sprite imageReturn = null;
+
+		foreach (QuestList ql in Quest_Database.Quests)
+		{
+			if (ql.Quest_ID == Quest_ID)
+			{
+				imageReturn = ql.Image; 
+			}
+		}
+		return imageReturn; 
+
+	}
+
+}
---
+++ Egyptian Treasure Quest Z/Assets/_MyScripts/Optimization/Scripts/Object&Damage/DeathByTimer.cs
@@ -0,0 +1,66 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI; 
+
+[RequireComponent(typeof(TimerObjective))]
+public class DeathByTimer : MonoBehaviour
+{
+	public GameObject Timer_ui; 
+	public int Timer_Threshold;
+	public enum _Threshold_Type
+	{
+		Seconds,
+		Minutes
+	}
+
+	public _Threshold_Type  Threshold_Type;
+
+	bool Activated = false;
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () 
+	{
+		if (Timer_ui != null) {
+			if (!Timer_ui.activeSelf) {
+				Timer_ui.SetActive (true);  
+			}
+
+			if (GetComponent<TimerObjective> ().Timer_Type == TimerObjective.TypeTimer.Increasing) {
+				if (Threshold_Type == _Threshold_Type.Seconds) {
+					if (Timer_Threshold <= GetComponent<TimerObjective> ().Seconds) {
+						StephGameManager.instance.Lose = true;   
+					}
+				}
+
+				if (Threshold_Type == _Threshold_Type.Minutes) {
+					if (Timer_Threshold <= GetComponent<TimerObjective> ().Minute) {
+						StephGameManager.instance.Lose = true;   
+					}
+				}
+				Timer_ui.GetComponent<Text> ().text = GetComponent<TimerObjective> ().Minute.ToString () + " : " + GetComponent<TimerObjective> ().Seconds.ToString (); 
+			}
+
+			if (GetComponent<TimerObjective> ().Timer_Type == TimerObjective.TypeTimer.Decreasing) {
+				if (Threshold_Type == _Threshold_Type.Seconds) {
+					if (Timer_Threshold >= GetComponent<TimerObjective> ().Decreasing_Secs) {
+						StephGameManager.instance.Lose = true;   
+					}
+				}
+
+				if (Threshold_Type == _Threshold_Type.Minutes) {
+					if (Timer_Threshold >= GetComponent<TimerObjective> ().Decreasing_Minute) {
+						StephGameManager.instance.Lose = true;   
+					}
+				}
+				Timer_ui.GetComponent<Text> ().text = GetComponent<TimerObjective> ().Decreasing_Minute.ToString () + " : " + GetComponent<TimerObjective> ().Decreasing_Secs.ToString ();
+			}
+
+		} else {
+			Debug.LogWarning ("Wala naka Set Timer UI mo"); 
+		}
+	}
+}
